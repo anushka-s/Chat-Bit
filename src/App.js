abfd903a711584704cb66ca49769d79515ecf295
AnnaSrivastava <ananyavsrivastava@gmail.com>
2020-10-01 09:06:25 +0530

    Just added a new state param

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Layout from './components/Layout'
-
-function App() {
+import Layout from './components/Layout';
+//stateless component
+class App extends React.Component {
+	render(){
   return (
-      <Layout title="Chat Bit App"/>
+      <Layout title="Chat App"/>
   );
+}
 }
 
 export default App;
